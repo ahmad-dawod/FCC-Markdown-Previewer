1ae5b4c8836d6bd99c388b693023a0d250006f9c
ahmad alborene <ahmad.mamoun.dawoud@gmail.com>
2023-11-20 21:39:56 +0300

    Full project

---
+++ markdowm-previewer/src/App.tsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+import './App.css'
+import ReactMarkdown from 'react-markdown'
+
+const defaultMarkdown = `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... **_both!_**
+
+There's also [links](https://codepen.io/ahmad-dawoud/pen/NWjjvxe), and
+> Block Quotes!
+
+![a Logo w/ Text](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAHXPluq6GtTRPDIHRv5kJPy86uFjp5sO7hg&usqp=CAU)
+
+- And of course there are lists.
+  - Some are bulleted.
+      - With different indentation levels.
+        - That look like this.
+`;
+
+function App() {
+  const [markdownText, setMarkdownText] = useState<string>(defaultMarkdown)
+  const deale = document.querySelector("#Ediv")
+  const dealp = document.querySelector("#preview")
+  const funp = function(){
+    deale?.classList.toggle("hiad")
+  }
+  const fune = function(){
+    dealp?.classList.toggle("hiad")
+  }
+
+  return (
+    <>
+      <div><h1>Markdown Previewer</h1></div>
+      <div className='boxes-container'>
+        <div id='Ediv'>
+        <p id="heade" onClick={fune}>Editor</p>
+        <textarea name="editor" id="editor" value={markdownText} onChange={(e) => setMarkdownText(e.target.value)}></textarea>
+        </div>
+        <div id="preview">
+          <p id="headp" onClick={funp}>Previewer</p>
+          <div id="pre">
+          <ReactMarkdown>{markdownText}</ReactMarkdown>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
